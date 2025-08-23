1083c6a8bc19f0328d6cba1520b36f3dec0a8f9f
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-23 04:47:32 +0100

    Create admin.js

---
+++ backend/permissions/roles/admin.js
@@ -0,0 +1,8 @@
+module.exports = {
+  role: 'admin',
+  permissions: [
+    'read_users','write_users','delete_users',
+    'manage_products','manage_orders',
+    'manage_roles','manage_settings'
+  ]
+};
