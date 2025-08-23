40495b9b2b17eedf462f4fb02cdc088ad8f39b62
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-23 15:00:33 +0100

    Create app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,23 +1,11 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminDashboard from './components/dashboard/AdminDashboard';
-import SellerDashboard from './components/dashboard/SellerDashboard';
-import CustomerDashboard from './components/dashboard/CustomerDashboard';
-import Header from './components/header/Header';
+import React from "react";
+import { AuthProvider } from "./context/AuthContext";
+import SellerPage from "../pages/seller";
 
-function App() {
-  const [userType, setUserType] = useState('customer'); // admin/seller/customer
-
+export default function App() {
   return (
-    <Router>
-      <Header userType={userType} setUserType={setUserType} />
-      <Routes>
-        {userType === 'admin' && <Route path="/" element={<AdminDashboard />} />}
-        {userType === 'seller' && <Route path="/" element={<SellerDashboard />} />}
-        {userType === 'customer' && <Route path="/" element={<CustomerDashboard />} />}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <SellerPage />
+    </AuthProvider>
   );
 }
-
-export default App;
