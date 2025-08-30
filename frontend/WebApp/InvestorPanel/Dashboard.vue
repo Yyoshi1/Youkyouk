8fc7edd9682499c5ba45862c6af98bc9b471d003
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-08-30 18:44:17 +0100

    Create Dashboard.vue

---
+++ frontend/WebApp/InvestorPanel/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="investor-dashboard">
+    <Header :title="'Investor Dashboard'" />
+    <Sidebar :links="sidebarLinks" />
+    <MainContent>
+      <InvestorOverview />
+    </MainContent>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Sidebar from '../components/Sidebar.vue'
+import MainContent from '../components/MainContent.vue'
+import InvestorOverview from './InvestorOverview.vue'
+
+export default {
+  name: 'InvestorDashboard',
+  components: { Header, Sidebar, MainContent, InvestorOverview },
+  data() {
+    return {
+      sidebarLinks: [
+        { name: 'Portfolio', path: '/portfolio' },
+        { name: 'Reports', path: '/reports' },
+        { name: 'Transactions', path: '/transactions' },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.investor-dashboard {
+  display: flex;
+  min-height: 100vh;
+}
+</style>
