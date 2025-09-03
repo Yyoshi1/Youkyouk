4f331e11dc4e1b69a96fb4392a7172ec172398ad
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 15:00:15 +0100

    Delete backup.sh

--- backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BACKUP_DIR="./backups"
-mkdir -p $BACKUP_DIR
-TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-pg_dump -U $DB_USER $DB_NAME > $BACKUP_DIR/youkyouk_$TIMESTAMP.sql
-echo "Backup completed at $BACKUP_DIR/youkyouk_$TIMESTAMP.sql"
