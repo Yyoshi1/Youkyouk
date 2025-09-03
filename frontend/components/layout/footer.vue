d895605dfb13547a42e23ad4998352045744d5a1
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 15:37:33 +0100

    Create footer.vue

---
+++ frontend/components/layout/footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <span>© 2025 Youkyouk Dashboard</span>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background: #1f2937;
+  color: white;
+  text-align: center;
+  padding: 15px;
+}
+</style>
