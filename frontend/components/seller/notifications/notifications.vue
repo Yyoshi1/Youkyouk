6f4acd0c44bb13780926d3152570b91a9935f690
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 13:46:55 +0100

    Create notifications.vue

---
+++ frontend/components/seller/notifications/notifications.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="notifications">
+    <Header :title="'Notifications'" :user="seller" @toggle-theme="toggleTheme" @toggle-sidebar="toggleSidebar"/>
+    <ul class="notification-list">
+      <li v-for="notif in notifications" :key="notif.id" :class="{ unread: !notif.read }">
+        <span>{{ notif.type }}: </span>
+        <span>{{ notif.message }}</span>
+        <button @click="markAsRead(notif)">Mark Read</button>
+      </li>
+    </ul>
+    <div v-if="unreadCount > 0" class="badge">{{ unreadCount }}</div>
