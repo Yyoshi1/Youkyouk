39c882db00a30708ad7d47eb15f52a51fe6e661c
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 03:02:58 +0100

    Create sidebar.vue

---
+++ frontend/components/layout/seller/sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <aside :class="['sidebar', { collapsed }]">
+    <ul>
+      <li v-for="item in menu" :key="item.route">
+        <router-link :to="item.route">
+          <span>{{ item.name }}</span>
+        </router-link>
+      </li>
+    </ul>
+    <button @click="$emit('toggle')">Toggle</button>
+  </aside>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({ menu:Array, collapsed:Boolean })
+</script>
+
+<style scoped>
+.sidebar {
+  width: 250px;
+  background: #1f2937;
+  color: white;
+  transition: width 0.3s;
+}
+.sidebar.collapsed { width: 60px; }
+.sidebar ul { list-style: none; padding: 0; }
+.sidebar li { padding: 15px; }
+</style>
