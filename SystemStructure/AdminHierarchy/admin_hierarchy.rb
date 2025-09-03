e82f0d442df30b4f4cfb094a8002f1ca58824843
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 01:03:39 +0100

    Delete SystemStructure directory

--- SystemStructure/AdminHierarchy/admin_hierarchy.rb
+++
@@ -1,9 +0,0 @@
-class AdminHierarchy
-  has_many :sub_admins, class_name: "Admin", foreign_key: "parent_id"
-  belongs_to :super_admin, class_name: "Admin", optional: true
-
-  def can_manage?(admin)
-    # 
-    admin.level < self.level
-  end
-end
--- SystemStructure/InvestorsLevels/investor_level.rb
+++
@@ -1,6 +0,0 @@
-class InvestorLevel
-  belongs_to :investor
-  enum level: { super_admin: 0, continent_admin: 1, country_admin: 2, model_admin: 3 }
-
-  validates :level, presence: true
-end
