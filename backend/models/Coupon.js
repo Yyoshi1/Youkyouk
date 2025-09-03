fa0c42bbc5c68284a5f4d9fc9bdc4775b948260f
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 02:34:55 +0100

    Update Coupon.js

--- backend/models/Coupon.js
+++ backend/models/Coupon.js
@@ -1,7 +1,18 @@
-import { Model } from "objection"
+import { Model } from 'objection'
+import { Product } from './Product'
 
 export class Coupon extends Model {
-  static get tableName() {
-    return "coupons"
+  static tableName = 'coupons'
+
+  static relationMappings = {
+    products: {
+      relation: Model.ManyToManyRelation,
+      modelClass: Product,
+      join: {
+        from: 'coupons.id',
+        through: { from: 'coupon_products.coupon_id', to: 'coupon_products.product_id' },
+        to: 'products.id'
+      }
+    }
   }
 }
