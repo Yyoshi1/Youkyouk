e2811b075ecdb393266e4cf8311503739d05902d
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 01:35:17 +0100

    Create 20230903_create_orders.js

---
+++ backend/migrate/20230903_create_orders.js
@@ -0,0 +1,18 @@
+export async function up(knex) {
+  await knex.schema.createTable("orders", (table) => {
+    table.increments("id").primary()
+    table
+      .integer("user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+    table.decimal("total", 10, 2).notNullable()
+    table.enum("status", ["pending", "paid", "shipped", "completed", "canceled"]).defaultTo("pending")
+    table.timestamps(true, true)
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable("orders")
+}
