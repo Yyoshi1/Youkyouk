4545c53a73de3136aacc2758383966cf495830f7
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 15:04:58 +0100

    Create header.vue

---
+++ frontend/components/layout/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <div class="left">
+      <button @click="$emit('toggle-sidebar')">
+        <i class="linear-icon-menu"></i>
+      </button>
+      <h1>{{ title }}</h1>
+    </div>
+    <div class="right">
+      <div class="notifications" @click="toggleDropdown">
+        <i class="linear-icon-bell"></i>
+        <span v-if="unreadCount > 0" class="badge">{{ unreadCount }}</span>
+        <ul v-if="dropdownOpen">
+          <li v-for="note in notifications" :key="note.id" :class="{ unread: !note.read }">
+            {{ note.message }}
+          </li>
+        </ul>
+      </div>
+      <span>{{ user.name }}</span>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { ref, computed, on
