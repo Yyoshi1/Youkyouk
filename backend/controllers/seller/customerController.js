c0fccb1d6f0ef1ff7960644f12c706ec03f9ef65
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 15:23:11 +0100

    Create customerController.js

---
+++ backend/controllers/seller/customerController.js
@@ -0,0 +1,13 @@
+import { Customer } from "../../models/seller/Customer"
+
+export class CustomerController {
+  static async list(req, res) {
+    const customers = await Customer.query()
+    res.json(customers)
+  }
+
+  static async get(req, res) {
+    const customer = await Customer.query().findById(req.params.id)
+    res.json(customer)
+  }
+}
