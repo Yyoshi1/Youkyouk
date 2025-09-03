f0eabe196b340a3ca82598538d8511e819bec136
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 14:14:50 +0100

    Update product.js

--- backend/models/seller/product.js
+++ backend/models/seller/product.js
@@ -1,4 +1,3 @@
-// backend/models/seller/Product.js
 import { Model } from "objection"
 
 export class Product extends Model {
@@ -6,25 +5,8 @@ export class Product extends Model {
 
   static get relationMappings() {
     const { Seller } = require("./Seller")
-    const { Review } = require("./Review")
-    const { Module } = require("./Module")
-
     return {
-      seller: {
-        relation: Model.BelongsToOneRelation,
-        modelClass: Seller,
-        join: { from: "products.seller_id", to: "sellers.id" }
-      },
-      reviews: {
-        relation: Model.HasManyRelation,
-        modelClass: Review,
-        join: { from: "products.id", to: "reviews.product_id" }
-      },
-      module: {
-        relation: Model.BelongsToOneRelation,
-        modelClass: Module,
-        join: { from: "products.module_id", to: "modules.id" }
-      }
+      seller: { relation: Model.BelongsToOneRelation, modelClass: Seller, join: { from: "products.seller_id", to: "sellers.id" } }
     }
   }
 }
