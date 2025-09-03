f1377b78d0ad57785f9452a3e5ff160ed4eb7e43
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 14:47:36 +0100

    Update notification.js

--- backend/models/seller/notification.js
+++ backend/models/seller/notification.js
@@ -5,8 +5,10 @@ export class Notification extends Model {
 
   static get relationMappings() {
     const { Seller } = require("./Seller")
+    const { Order } = require("./Order")
     return {
-      seller: { relation: Model.BelongsToOneRelation, modelClass: Seller, join: { from: "notifications.seller_id", to: "sellers.id" } }
+      seller: { relation: Model.BelongsToOneRelation, modelClass: Seller, join: { from: "notifications.seller_id", to: "sellers.id" } },
+      order: { relation: Model.BelongsToOneRelation, modelClass: Order, join: { from: "notifications.order_id", to: "orders.id" } },
     }
   }
 }
