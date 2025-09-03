3cb977c99eca3ad082c5f9347706c8c025097786
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 14:25:00 +0100

    Update customer.js

--- backend/models/seller/customer.js
+++ backend/models/seller/customer.js
@@ -1,17 +1,18 @@
-// backend/models/seller/Customer.js
 import { Model } from "objection"
 
 export class Customer extends Model {
   static get tableName() { return "customers" }
 
   static get relationMappings() {
+    const { Seller } = require("./Seller")
     const { Order } = require("./Order")
+    const { Message } = require("./Message")
+    const { Review } = require("./Review")
     return {
-      orders: {
-        relation: Model.HasManyRelation,
-        modelClass: Order,
-        join: { from: "customers.id", to: "orders.customer_id" }
-      }
+      seller: { relation: Model.BelongsToOneRelation, modelClass: Seller, join: { from: "customers.seller_id", to: "sellers.id" } },
+      orders: { relation: Model.HasManyRelation, modelClass: Order, join: { from: "customers.id", to: "orders.user_id" } },
+      messages: { relation: Model.HasManyRelation, modelClass: Message, join: { from: "customers.id", to: "messages.user_id" } },
+      reviews: { relation: Model.HasManyRelation, modelClass: Review, join: { from: "customers.id", to: "reviews.user_id" } }
     }
   }
 }
