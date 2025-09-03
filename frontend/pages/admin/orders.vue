7bd81cef829fd8097b6d6193de73a15d9d3337eb
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-03 02:23:01 +0100

    Create orders.vue

---
+++ frontend/pages/admin/orders.vue
@@ -0,0 +1,19 @@
+<template>
+  <AdminLayout>
+    <template #default>
+      <h1>Orders</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Customer</th>
+            <th>Products</th>
+            <th>Total</th>
+            <th>Status</th>
+            <th>Created At</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="order in orders" :key="order.id">
+            <td>{{ order
