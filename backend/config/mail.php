5e129420301003e266440288f28b8249ba07bdfa
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 19:14:48 +0100

    Create mail.php

---
+++ backend/config/mail.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'from_address' => 'no-reply@youkyouk.com',
+    'from_name' => 'Youkyouk',
+    'smtp' => [
+        'host' => 'localhost',
+        'port' => 25,
+        'username' => '',
+        'password' => '',
+        'encryption' => null,
+    ],
+];
