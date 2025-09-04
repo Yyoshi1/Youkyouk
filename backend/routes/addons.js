f85b071b418cf56238331bf53140e1396ae6ebcf
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 18:28:30 +0100

    Delete backend directory

--- backend/routes/addons.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { getAddons, toggleAddon } from "../../controllers/seller/addons.js"
-
-const router = express.Router()
-
-router.get("/", getAddons)                // 
-router.post("/:id/toggle", toggleAddon)   // 
-
-export default router
