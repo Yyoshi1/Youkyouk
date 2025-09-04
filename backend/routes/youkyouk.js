7e17c1e804bcfe174fb09822ac4fb25127d893c5
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 04:20:46 +0100

    Update youkyouk.js

--- backend/routes/youkyouk.js
+++ backend/routes/youkyouk.js
@@ -2,10 +2,7 @@ import express from "express"
 import { getPageLayout, toggleAddon } from "../controllers/youkyoukController.js"
 const router = express.Router()
 
-// 
 router.get("/page-layout", getPageLayout)
-
-// 
 router.post("/addon/:id/toggle", toggleAddon)
 
 export default router
