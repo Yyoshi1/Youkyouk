a672b3eaed2c8b34f661d971ab12b273d3bdd099
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 19:13:53 +0100

    Create app.php

---
+++ backend/config/app.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'name' => 'Youkyouk',
+    'env' => 'production', // development / production
+    'debug' => false,
+    'url' => 'https://yourdomain.com',
+    'timezone' => 'Africa/Casablanca',
+    'locale' => 'fr',
+    'fallback_locale' => 'en',
+];
