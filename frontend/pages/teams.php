3534ec01d9045c1190f2e80bf144c73495294625
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 20:08:10 +0100

    Create teams.php

---
+++ frontend/pages/teams.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Teams - Youkyouk
+ */
+include '../components/layout/Header.php';
+include '../components/layout/Sidebar.php';
+?>
+
+<div class="main-content">
+    <h1>Teams</h1>
+    <p>List of teams will appear here.</p>
+</div>
+
+<?php include '../components/layout/Footer.php'; ?>
