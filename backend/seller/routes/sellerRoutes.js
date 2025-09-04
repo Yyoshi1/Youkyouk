f4f0c0d8cf63d2733e56c912e3c990a8b740b560
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 03:49:53 +0100

    Create sellerRoutes.js

---
+++ backend/seller/routes/sellerRoutes.js
@@ -0,0 +1,27 @@
+import express from "express"
+import {
+  createSeller,
+  getSellers,
+  getSellerById,
+  updateSeller,
+  deleteSeller
+} from "../controllers/sellerController.js"
+
+const router = express.Router()
+
+// ✅ 
+router.post("/", createSeller)
+
+// ✅ 
+router.get("/", getSellers)
+
+// ✅ 
+router.get("/:id", getSellerById)
+
+// ✅ 
+router.put("/:id", updateSeller)
+
+// ✅ 
+router.delete("/:id", deleteSeller)
+
+export default router
