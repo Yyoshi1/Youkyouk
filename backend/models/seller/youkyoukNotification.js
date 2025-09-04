8c9760b1705d8f67801cd2348ecd9f3b294a9750
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 05:53:41 +0100

    Create youkyoukNotification.js

---
+++ backend/models/seller/youkyoukNotification.js
@@ -0,0 +1,13 @@
+import { Model } from "objection"
+
+export class YoukyoukNotification extends Model {
+  static get tableName() { return "notifications" }
+
+  static get relationMappings() {
+    const { YoukyoukSeller } = require("./YoukyoukSeller")
+
+    return {
+      seller: { relation: Model.BelongsToOneRelation, modelClass: YoukyoukSeller, join: { from: "notifications.seller_id", to: "sellers.id" } }
+    }
+  }
+}
