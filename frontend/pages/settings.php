a302c8f6e136f14450bc2ab5c3855ea88a7a319b
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 20:08:31 +0100

    Create settings.php

---
+++ frontend/pages/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Settings - Youkyouk
+ */
+include '../components/layout/Header.php';
+include '../components/layout/Sidebar.php';
+?>
+
+<div class="main-content">
+    <h1>Settings</h1>
+    <p>System and user settings will appear here.</p>
+</div>
+
+<?php include '../components/layout/Footer.php'; ?>
