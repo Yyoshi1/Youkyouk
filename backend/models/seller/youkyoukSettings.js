5697d1f7159b48d436d4e4f3b9d79640e2fad87a
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 05:54:15 +0100

    Create youkyoukSettings.js

---
+++ backend/models/seller/youkyoukSettings.js
@@ -0,0 +1,13 @@
+import { Model } from "objection"
+
+export class YoukyoukSettings extends Model {
+  static get tableName() { return "settings" }
+
+  static get relationMappings() {
+    const { YoukyoukSeller } = require("./YoukyoukSeller")
+
+    return {
+      seller: { relation: Model.BelongsToOneRelation, modelClass: YoukyoukSeller, join: { from: "settings.seller_id", to: "sellers.id" } }
+    }
+  }
+}
