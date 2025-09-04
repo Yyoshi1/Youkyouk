1c1f4b61d2a64f6294ceb419c3a58d8b23100dad
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 23:58:06 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
-// index.php - الملف الرئيسي للواجهة
+// index.php 
 
-// تفعيل عرض الأخطاء أثناء التطوير
+// 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// تضمين الهيدر
+// 
 $headerPath = __DIR__ . '/../frontend/components/layout/Header.php';
 if (file_exists($headerPath)) {
     include_once $headerPath;
@@ -14,7 +14,7 @@ if (file_exists($headerPath)) {
     echo "<p style='color:red;'>Header file not found: $headerPath</p>";
 }
 
-// --- محتوى الصفحة الرئيسي ---
+// --- ---
 ?>
 
 <div style="text-align:center; margin-top:50px;">
@@ -23,7 +23,7 @@ if (file_exists($headerPath)) {
 </div>
 
 <?php
-// تضمين الفوتر
+// 
 $footerPath = __DIR__ . '/../frontend/components/layout/Footer.php';
 if (file_exists($footerPath)) {
     include_once $footerPath;
