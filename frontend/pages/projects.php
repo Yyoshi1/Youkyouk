9a7f3164c925428083d0da7ea7ffa9a020b39c26
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 20:07:16 +0100

    Create projects.php

---
+++ frontend/pages/projects.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Projects - Youkyouk
+ */
+include '../components/layout/Header.php';
+include '../components/layout/Sidebar.php';
+?>
+
+<div class="main-content">
+    <h1>Projects</h1>
+    <p>List of projects will appear here.</p>
+</div>
+
+<?php include '../components/layout/Footer.php'; ?>
