a32a0b20ef7deafe09c06381974625ad78180c98
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 04:54:27 +0100

    Update GTM.js

--- backend/models/addons/GTM.js
+++ backend/models/addons/GTM.js
@@ -1,12 +1,4 @@
 import { Model } from "objection"
-
 export class GTM extends Model {
   static get tableName() { return "gtm_addon" }
-
-  static get relationMappings() {
-    const { Settings } = require("./Settings")
-    return {
-      settings: { relation: Model.HasOneRelation, modelClass: Settings, join: { from: "gtm_addon.id", to: "settings.addon_id" } }
-    }
-  }
 }
