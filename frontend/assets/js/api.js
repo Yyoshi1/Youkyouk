4d302c9683a7809267330e61fc9c6947110e70c4
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-04 21:01:06 +0100

    Update api.js

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,1 +1,10 @@
-
+function sendRequest(action, data, callback){
+    fetch('../api/ajax.php', {
+        method: 'POST',
+        headers: { 'Content-Type':'application/json' },
+        body: JSON.stringify({action, ...data})
+    })
+    .then(res => res.json())
+    .then(callback)
+    .catch(err => console.error(err));
+}
