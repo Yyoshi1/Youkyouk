e606af86a13dff013ba92af3f94f8ad7ada4e1dc
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-05 22:19:59 +0100

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,6 +4,7 @@ import Header from '../components/layout/Header'
 import DashboardCards from '../components/dashboard/DashboardCards'
 import CommandPalette from '../components/layout/CommandPalette'
 import CreateProjectModal from '../components/modals/CreateProjectModal'
+import TasksTable from '../components/dashboard/TasksTable'
 
 const DashboardPage: React.FC = () => {
   const [modalOpen, setModalOpen] = useState(false)
@@ -15,6 +16,7 @@ const DashboardPage: React.FC = () => {
         <Header />
         <main className="p-6 space-y-6">
           <DashboardCards />
+
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="h-64 bg-white dark:bg-gray-800 rounded shadow flex items-center justify-center text-gray-400">
               Chart 1
@@ -23,9 +25,10 @@ const DashboardPage: React.FC = () => {
               Chart 2
             </div>
           </div>
-          <div className="bg-white dark:bg-gray-800 rounded shadow p-4 text-gray-400">
-            Tasks / Table placeholder
-          </div>
+
+          {/* Tasks Table */}
+          <TasksTable />
+
           <button
             onClick={() => setModalOpen(true)}
             className="px-4 py-2 rounded bg-blue-500 hover:bg-blue-600 text-white transition"
@@ -35,7 +38,6 @@ const DashboardPage: React.FC = () => {
         </main>
       </div>
 
-      {/* Global Components */}
       <CommandPalette />
       <CreateProjectModal open={modalOpen} onClose={() => setModalOpen(false)} />
     </div>
