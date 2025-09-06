a1ede8667806c45d5cc7f7b63d04314b384908ab
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-06 13:19:34 +0100

    Create page.tsx

---
+++ frontend/app/settings/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SettingsPage: React.FC = () => {
+  return <div className="space-y-4">Youkyouk Settings content goes here...</div>
+}
+
+export default SettingsPage
