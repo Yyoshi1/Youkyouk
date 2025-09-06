5a3cc840f14116ece0a53d10f84554699be5adc9
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-06 13:17:40 +0100

    Create page.tsx

---
+++ frontend/app/projects/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ProjectsPage: React.FC = () => {
+  return <div className="space-y-4">Youkyouk Projects content goes here...</div>
+}
+
+export default ProjectsPage
