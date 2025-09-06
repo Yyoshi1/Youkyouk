31ed07d251d6c30a6011509155d8f7aadda7d859
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-06 14:28:10 +0100

    Update dashboard.tsx

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -3,17 +3,21 @@ import Sidebar from '../components/layout/Sidebar'
 import Header from '../components/layout/Header'
 import DashboardCards from '../components/dashboard/DashboardCards'
 import SearchModal from '../components/layout/SearchModal'
+import Footer from '../components/layout/Footer'
 
 const DashboardPage: React.FC = () => {
   return (
-    <div className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <main className="p-6">
-          <DashboardCards />
-        </main>
+    <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
+      <div className="flex flex-1">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <main className="p-6 flex-1">
+            <DashboardCards />
+          </main>
+        </div>
       </div>
+      <Footer />
       <SearchModal />
     </div>
   )
