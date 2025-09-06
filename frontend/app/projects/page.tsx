0e56a069dc4d309bc1a05c1b209a9f8f59f0724f
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-06 16:11:59 +0100

    Delete frontend/app/projects/page.tsx

--- frontend/app/projects/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ProjectsPage: React.FC = () => {
-  return <div className="space-y-4">Youkyouk Projects content goes here...</div>
-}
-
-export default ProjectsPage
