176c179c8d6e233be3775cb8ac1deef31c395e30
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-06 14:04:21 +0100

    Create dashboard.tsx

---
+++ frontend/pages/dashboard.tsx
@@ -0,0 +1,22 @@
+// frontend/pages/dashboard.tsx
+import React from 'react'
+import Sidebar from '../components/layout/Sidebar'
+import Header from '../components/layout/Header'
+import DashboardCards from '../components/dashboard/DashboardCards'
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="p-6">
+          <DashboardCards />
+          {/* هنا يمكن إضافة charts, tables, notifications مطابقة للينيار */}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardPage
