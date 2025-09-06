3cd39b2f72b8cddd28e2e7aefdfab874c2876e88
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-06 16:36:40 +0100

    Update dashboard.tsx

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -6,33 +6,24 @@ import Footer from '../components/layout/Footer'
 import DashboardCards from '../components/dashboard/DashboardCards'
 import ChartsSection from '../components/dashboard/ChartsSection'
 import TasksTable from '../components/dashboard/TasksTable'
+import SearchModal from '../components/dashboard/SearchModal' // هنا استدعاء SearchModal
 
 const DashboardPage: React.FC = () => {
   return (
     <div className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* Main content */}
       <div className="flex-1 flex flex-col">
-        {/* Header */}
         <Header />
-
-        {/* Dashboard sections */}
-        <main className="flex-1 overflow-auto p-6 space-y-6">
-          {/* Cards Section */}
+        <main className="p-6 flex-1 overflow-auto space-y-6">
           <DashboardCards />
-
-          {/* Charts Section */}
           <ChartsSection />
-
-          {/* Tasks Table Section */}
           <TasksTable />
         </main>
-
-        {/* Footer */}
         <Footer />
       </div>
+
+      {/* Search Modal يجب وضعه هنا خارج الهيكل الرئيسي حتى يغطي كامل الصفحة */}
+      <SearchModal />
     </div>
   )
 }
