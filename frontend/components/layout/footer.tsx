79104e176742e1f35dd9e332f19f0418979a9338
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-06 13:41:18 +0100

    Update footer.tsx

--- frontend/components/layout/footer.tsx
+++ frontend/components/layout/footer.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { HeartIcon } from '@heroicons/react/24/solid'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full py-4 px-6 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 text-gray-600 dark:text-gray-300 flex justify-center items-center">
-      <span>
-        Made with <HeartIcon className="w-4 h-4 text-red-500 inline-block mx-1" /> by Youkyouk Team
-      </span>
+    <footer className="p-4 bg-white dark:bg-gray-900 shadow text-center text-gray-500 dark:text-gray-400">
+      &copy; 2025 Youkyouk. All Rights Reserved.
     </footer>
   )
 }
