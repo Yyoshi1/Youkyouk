8ac9c68d47671290f9a8b81041a6192df7a5ca6e
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-06 14:45:41 +0100

    Update dashboard.tsx

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,9 +1,9 @@
-// /frontend/pages/dashboard.tsx
 import React from 'react'
 import Sidebar from '../components/layout/Sidebar'
 import Header from '../components/layout/Header'
 import DashboardCards from '../components/dashboard/DashboardCards'
-import SearchModal from '../components/dashboard/SearchModal'
+import ChartsSection from '../components/dashboard/ChartsSection'
+import TasksTable from '../components/dashboard/TasksTable'
 
 const DashboardPage: React.FC = () => {
   return (
@@ -11,12 +11,13 @@ const DashboardPage: React.FC = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="p-6 flex-1 overflow-y-auto">
+        <main className="p-6">
           <DashboardCards />
-          {/* هنا يمكن إضافة مزيد من محتوى Dashboard مثل charts, tables */}
+          <ChartsSection />
+          <TasksTable />
+          {/* يمكن إضافة أي محتوى إضافي مطابق للينيار هنا */}
         </main>
       </div>
-      <SearchModal />
     </div>
   )
 }
