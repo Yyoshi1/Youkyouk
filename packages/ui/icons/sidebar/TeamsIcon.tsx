3233adf008fbf81c4ef17e78f1c847f59bb4d940
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 23:45:10 +0100

    Create TeamsIcon.tsx

---
+++ packages/ui/icons/sidebar/TeamsIcon.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const TeamsIcon: React.FC<{ className?: string }> = ({ className }) => (
+  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a4 4 0 00-3-3.87M9 20H4v-2a4 4 0 013-3.87M12 4a4 4 0 110 8 4 4 0 010-8z"/>
+  </svg>
+);
