13d2fff9a104e0c3152c35264c6160c2721c87d6
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 00:27:21 +0100

    Update Sidebar.tsx

--- packages/ui/components/Sidebar.tsx
+++ packages/ui/components/Sidebar.tsx
@@ -1,17 +1,8 @@
-// packages/ui/components/Sidebar.tsx
 import React from "react";
 import {
-  InboxIcon,
-  IssuesIcon,
-  PulseIcon,
-  WorkspaceIcon,
-  InitiativesIcon,
-  ProjectsIcon,
-  ViewsIcon,
-  MembersIcon,
-  TeamsIcon,
-  FavoritesIcon,
-  YourTeamIcon,
+  InboxIcon, IssuesIcon, PulseIcon, WorkspaceIcon,
+  InitiativesIcon, ProjectsIcon, ViewsIcon, MembersIcon,
+  TeamsIcon, FavoritesIcon, YourTeamIcon
 } from "../icons/Icons";
 
 interface SidebarItem {
@@ -47,29 +38,27 @@ const sidebarItems: SidebarItem[] = [
   },
 ];
 
-export const Sidebar: React.FC = () => {
-  return (
-    <aside className="w-64 bg-white dark:bg-gray-800 shadow flex-shrink-0 h-screen overflow-auto">
-      <ul className="p-4 space-y-2">
-        {sidebarItems.map((item, idx) => (
-          <li key={idx}>
-            <div className="flex items-center space-x-2 p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
-              <item.icon className="w-5 h-5 text-gray-700 dark:text-gray-300" />
-              <span className="text-gray-900 dark:text-white font-medium">{item.title}</span>
-            </div>
-            {item.subItems && (
-              <ul className="pl-6 mt-1 space-y-1">
-                {item.subItems.map((sub, subIdx) => (
-                  <li key={subIdx} className="flex items-center space-x-2 p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
-                    <sub.icon className="w-4 h-4 text-gray-600 dark:text-gray-400" />
-                    <span className="text-gray-800 dark:text-gray-200">{sub.title}</span>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
-    </aside>
-  );
-};
+export const Sidebar: React.FC = () => (
+  <aside className="w-64 bg-white dark:bg-gray-800 shadow flex-shrink-0 h-screen overflow-auto">
+    <ul className="p-4 space-y-2">
+      {sidebarItems.map((item, idx) => (
+        <li key={idx}>
+          <div className="flex items-center space-x-2 p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+            <item.icon className="w-5 h-5 text-gray-700 dark:text-gray-300" />
+            <span className="text-gray-900 dark:text-white font-medium">{item.title}</span>
+          </div>
+          {item.subItems && (
+            <ul className="pl-6 mt-1 space-y-1">
+              {item.subItems.map((sub, subIdx) => (
+                <li key={subIdx} className="flex items-center space-x-2 p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+                  <sub.icon className="w-4 h-4 text-gray-600 dark:text-gray-400" />
+                  <span className="text-gray-800 dark:text-gray-200">{sub.title}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+  </aside>
+);
