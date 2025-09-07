f5631a1f2cb0b3a52d9c1c235b799c6841317c09
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 22:45:23 +0100

    Update Input.tsx

--- packages/ui/components/Input.tsx
+++ packages/ui/components/Input.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
 
-export const Input: React.FC<InputProps> = (props) => {
+export const Input: React.FC<InputProps> = ({ label, ...props }) => {
   return (
-    <input
-      {...props}
-      className="px-3 py-2 border border-gray-300 rounded w-full focus:outline-none focus:ring focus:border-blue-500"
-    />
+    <div className="flex flex-col mb-4">
+      {label && <label className="mb-1 text-gray-700 dark:text-gray-300">{label}</label>}
+      <input
+        className="px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white"
+        {...props}
+      />
+    </div>
   );
 };
