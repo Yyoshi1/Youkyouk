63036bfb17d2ab595c55a0dc8d16ecf1b658ba6a
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 19:33:00 +0100

    Update Sidebar.tsx

--- packages/ui/components/Sidebar.tsx
+++ packages/ui/components/Sidebar.tsx
@@ -1,10 +1,12 @@
-// packages/ui/components/Sidebar.tsx
 import React from "react";
+import Link from "next/link";
 
 export const Sidebar: React.FC = () => {
   return (
-    <aside className="w-64 bg-gray-100 dark:bg-gray-900 p-4">
-      <p className="text-gray-900 dark:text-white">Sidebar</p>
+    <aside className="w-64 bg-gray-100 dark:bg-gray-900 h-screen p-4 space-y-2">
+      <Link href="/dashboard"><a className="block p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">Dashboard</a></Link>
+      <Link href="/projects"><a className="block p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">Projects</a></Link>
+      <Link href="/teams"><a className="block p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">Teams</a></Link>
     </aside>
   );
 };
