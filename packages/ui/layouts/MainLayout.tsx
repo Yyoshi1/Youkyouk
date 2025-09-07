60f1b648006f09c9eba52c12fea827b328332cf9
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 06:25:12 +0100

    Update MainLayout.tsx

--- packages/ui/layouts/MainLayout.tsx
+++ packages/ui/layouts/MainLayout.tsx
@@ -1,27 +1,23 @@
 import React from "react";
-import { YoukyoukHeader } from "../YoukyoukHeader";
-import { YoukyoukSidebar } from "../YoukyoukSidebar";
-import { ThemeProvider } from "../context/ThemeContext";
+import { Header } from "./Header";
 
-interface Props {
+interface MainLayoutProps {
   children: React.ReactNode;
 }
 
-export const MainLayout: React.FC<Props> = ({ children }) => {
+export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <ThemeProvider>
-      <div className="flex flex-col h-screen bg-neutral-50 dark:bg-neutral-900">
-        <div className="flex flex-1 overflow-hidden">
-          <YoukyoukSidebar />
-          <div className="flex-1 flex flex-col">
-            <YoukyoukHeader />
-            <main className="p-6 overflow-auto flex-1">{children}</main>
-          </div>
-        </div>
-        <footer className="h-12 bg-white dark:bg-neutral-800 border-t flex items-center justify-center text-xs text-neutral-400">
-          © 2025 youkyouk. All rights reserved.
-        </footer>
-      </div>
-    </ThemeProvider>
+    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
+      {/*  */}
+      <Header />
+
+      {/* */}
+      <main className="flex-1 p-4">{children}</main>
+
+      {/* */}
+      <footer className="p-4 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 text-center">
+        © 2025 Youkyouk. All rights reserved.
+      </footer>
+    </div>
   );
 };
