6214e7a7561c1513e2b5ce9131ae51c169c6b3b2
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 05:50:28 +0100

    Create AdminLayout.tsx

---
+++ packages/ui/layouts/AdminLayout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+import { AdminSidebar } from "../components/AdminSidebar";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
+interface AdminLayoutProps {
+  children: ReactNode;
+}
+
+export const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
+  return (
+    <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+      <AdminSidebar />
+      <div className="flex flex-col flex-1">
+        <Header />
+        <main className="flex-1 p-6 overflow-auto">{children}</main>
+        <Footer />
+      </div>
+    </div>
+  );
+};
