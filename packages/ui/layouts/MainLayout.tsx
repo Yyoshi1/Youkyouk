5ad0a18ca7a7a6cb5b958287a2cc41c407f0010b
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 22:49:48 +0100

    Update MainLayout.tsx

--- packages/ui/layouts/MainLayout.tsx
+++ packages/ui/layouts/MainLayout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
-import { Footer } from "../components/Footer";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -9,13 +8,14 @@ interface MainLayoutProps {
 
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
-      <Header />
-      <div className="flex flex-1">
-        <Sidebar />
+    <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+      <Sidebar />
+
+      <div className="flex flex-col flex-1">
+        <Header />
+
         <main className="flex-1 p-6 overflow-auto">{children}</main>
       </div>
-      <Footer />
     </div>
   );
 };
