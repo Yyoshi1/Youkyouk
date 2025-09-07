428d721ec700350919f71390312ff1daedc2b2d5
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 06:37:48 +0100

    Update MainLayout.tsx

--- apps/web/packages/ui/layouts/MainLayout.tsx
+++ apps/web/packages/ui/layouts/MainLayout.tsx
@@ -1,23 +1,29 @@
 import React from "react";
-import { Sidebar } from "../components/Sidebar";
-import { Footer } from "../components/Footer";
+import { Header } from "./Header";
+import { Sidebar } from "./Sidebar";
 
 interface MainLayoutProps {
   children: React.ReactNode;
-  activePage?: string;
 }
 
-export const MainLayout: React.FC<MainLayoutProps> = ({
-  children,
-  activePage,
-}) => {
+export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex flex-1">
-        <Sidebar activePage={activePage} />
-        <main className="flex-1 p-6 bg-gray-100">{children}</main>
+    <div className="min-h-screen flex bg-gray-50 dark:bg-gray-900">
+      {/* Sidebar */}
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+        {/* Header */}
+        <Header />
+
+        {/*  */}
+        <main className="flex-1 p-4 overflow-auto">{children}</main>
+
+        {/* Footer */}
+        <footer className="p-4 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 text-center">
+          © 2025 Youkyouk. All rights reserved.
+        </footer>
       </div>
-      <Footer />
     </div>
   );
 };
