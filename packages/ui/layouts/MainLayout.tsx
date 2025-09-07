024ce81311a0a8492a80d5e1083c7397dba08986
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 03:51:42 +0100

    Update MainLayout.tsx

--- packages/ui/layouts/MainLayout.tsx
+++ packages/ui/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { YoukyoukHeader } from "../YoukyoukHeader";
 import { YoukyoukSidebar } from "../YoukyoukSidebar";
+import { ThemeProvider } from "../context/ThemeContext";
 
 interface Props {
   children: React.ReactNode;
@@ -8,18 +9,19 @@ interface Props {
 
 export const MainLayout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="flex flex-col h-screen bg-neutral-50">
-      <div className="flex flex-1 overflow-hidden">
-        <YoukyoukSidebar />
-        <div className="flex-1 flex flex-col">
-          <YoukyoukHeader />
-          <main className="p-6 overflow-auto flex-1">{children}</main>
+    <ThemeProvider>
+      <div className="flex flex-col h-screen bg-neutral-50 dark:bg-neutral-900">
+        <div className="flex flex-1 overflow-hidden">
+          <YoukyoukSidebar />
+          <div className="flex-1 flex flex-col">
+            <YoukyoukHeader />
+            <main className="p-6 overflow-auto flex-1">{children}</main>
+          </div>
         </div>
+        <footer className="h-12 bg-white dark:bg-neutral-800 border-t flex items-center justify-center text-xs text-neutral-400">
+          © 2025 youkyouk. All rights reserved.
+        </footer>
       </div>
-      {/* Footer */}
-      <footer className="h-12 bg-white border-t flex items-center justify-center text-xs text-neutral-400">
-        © 2025 youkyouk. All rights reserved.
-      </footer>
-    </div>
+    </ThemeProvider>
   );
 };
