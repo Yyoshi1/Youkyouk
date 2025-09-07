ec29dd22ca67c183cb23e5efe308b4dc56a32074
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 21:10:28 +0100

    Delete packages/ui directory

--- packages/ui/license/LicenseManager.ts
+++
@@ -1,6 +0,0 @@
-export class LicenseManager {
-  private static LICENSE_KEY = "YOUR_SALES_KEY_HERE"; 
-  static validate(key: string): boolean {
-    return key === LicenseManager.LICENSE_KEY;
-  }
-}
