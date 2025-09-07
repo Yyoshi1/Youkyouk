21ef4ff88121bc73b3485837460c89bce2c62874
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 15:53:55 +0100

    Create Header.tsx

---
+++ packages/ui/layouts/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <header className="bg-white dark:bg-gray-800 shadow p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold text-gray-900 dark:text-white">Youkyouk</h1>
+    </header>
+  );
+};
