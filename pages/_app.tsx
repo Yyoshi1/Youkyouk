570e38ac02e1b2d2a56b8bf024abea1001e179ba
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 21:48:39 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,11 @@
+// pages/_app.tsx
+import '../styles/globals.css';
+import { MainLayout } from '../packages/ui/layouts/MainLayout';
 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
+}
