613ee37ff25b57bad0d4d741450a44ac24c57347
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 19:37:02 +0100

    Delete packages/ui/layouts/Footer.tsx

--- packages/ui/layouts/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer: React.FC = () => {
-  return (
-    <footer className="bg-white dark:bg-gray-800 shadow p-4 text-center text-gray-500 dark:text-gray-400">
-      © 2025 Youkyouk. All rights reserved.
-    </footer>
-  );
-};
