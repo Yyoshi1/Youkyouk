47be1d46241eb1c274be95945d9eda88be6bb342
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 23:41:29 +0100

    Create NotificationIcon.tsx

---
+++ packages/ui/icons/header/NotificationIcon.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const NotificationsIcon: React.FC<{ className?: string }> = ({ className }) => (
+  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405M19 13V6a2 2 0 00-2-2H7a2 2 0 00-2 2v7m14 0l-1.405 1.405M5 17h5"/>
+  </svg>
+);
