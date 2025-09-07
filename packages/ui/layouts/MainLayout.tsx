8cbff933602372d372d9a94fdcbd8a7db22d6224
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 19:46:20 +0100

    Update MainLayout.tsx

--- packages/ui/layouts/MainLayout.tsx
+++ packages/ui/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
 import { Sidebar } from "../components/Sidebar";
+import { Footer } from "../components/Footer";
 
 interface MainLayoutProps {
   children: ReactNode;
