b6efa2f1da71c6a0d64c137fd179b6a518a570f6
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 20:07:27 +0100

    Update QuickSearch.tsx

--- packages/ui/components/QuickSearch.tsx
+++ packages/ui/components/QuickSearch.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { Input } from "./Input";
+import React, { forwardRef } from "react";
 
-export const QuickSearch: React.FC = () => {
-  return (
-    <div>
-      <Input placeholder="Search..." />
-    </div>
-  );
-};
+interface QuickSearchProps {
+  inputRef?: React.Ref<HTMLInputElement>;
+}
+
+export const QuickSearch = forwardRef<HTMLInputElement, QuickSearchProps>(
+  ({ inputRef }, ref) => {
+    return (
+      <input
+        ref={inputRef || ref}
+        type="text"
+        placeholder="Search... (Ctrl+E)"
+        className="w-full px-4 py-2 border rounded shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+      />
+    );
+  }
+);
+
+QuickSearch.displayName = "QuickSearch";
