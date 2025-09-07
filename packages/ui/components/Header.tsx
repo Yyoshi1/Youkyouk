c9c4235e662764c8a708458f8c9643936d5ed171
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 00:03:42 +0100

    Update Header.tsx

--- packages/ui/components/Header.tsx
+++ packages/ui/components/Header.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { QuickSearch } from "./QuickSearch";
 import { UserDropdown } from "./UserDropdown";
-import { SearchIcon, NotificationsIcon } from "../icons/general";
+import { NotificationIcon, SettingsIcon } from "../icons/header";
 
-export const Header: React.FC = () => (
-  <header className="flex justify-between items-center p-4 bg-white dark:bg-gray-800 shadow">
-    <div className="flex items-center space-x-2">
-      <img src="/logo.png" alt="Logo" className="h-8 w-8" />
-      <span className="font-bold text-xl">Youkyouk</span>
-    </div>
+export const Header: React.FC = () => {
+  return (
+    <header className="flex items-center justify-between px-6 py-4 bg-white dark:bg-gray-800 shadow">
+      {/*  */}
+      <div className="flex items-center space-x-2">
+        <img src="/logo.png" alt="Logo" className="h-8 w-8" />
+        <span className="font-bold text-xl text-gray-900 dark:text-white">Dashboard</span>
+      </div>
 
-    <div className="flex-1 mx-4">
-      <QuickSearch />
-    </div>
+      {/*  */}
+      <div className="flex-1 px-4">
+        <QuickSearch />
+      </div>
 
-    <div className="flex items-center space-x-4">
-      <button><SearchIcon className="w-5 h-5" /></button>
-      <button><NotificationsIcon className="w-5 h-5" /></button>
-      <UserDropdown />
-    </div>
-  </header>
-);
+      {/*  */}
+      <div className="flex items-center space-x-4">
+        <button className="p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">
+          <NotificationIcon className="w-5 h-5" />
+        </button>
+        <button className="p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">
+          <SettingsIcon className="w-5 h-5" />
+        </button>
+        <UserDropdown />
+      </div>
+    </header>
+  );
+};
