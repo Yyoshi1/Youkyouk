f8fbfcc4054fa84d14115ba5a2b8f4c72558c110
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 22:18:51 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 // pages/_app.tsx
-import '../styles/globals.css';
-import { MainLayout } from '../packages/ui/layouts/MainLayout';
+import type { AppProps } from "next/app";
+import MainLayout from "../packages/ui/layouts/MainLayout";
+import AdminLayout from "../packages/ui/layouts/AdminLayout";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, router }: AppProps) {
+  //  AdminLayout
+  const isAdminPage = router.pathname.startsWith("/admin");
+
+  const Layout = isAdminPage ? AdminLayout : MainLayout;
+
   return (
-    <MainLayout>
+    <Layout>
       <Component {...pageProps} />
-    </MainLayout>
+    </Layout>
   );
 }
