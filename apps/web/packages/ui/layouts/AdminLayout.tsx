a653082ee3953f008920136eb7bc7c205ae2b3a4
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 19:18:03 +0100

    Update AdminLayout.tsx

--- apps/web/packages/ui/layouts/AdminLayout.tsx
+++ apps/web/packages/ui/layouts/AdminLayout.tsx
@@ -1,5 +1,3 @@
-// apps/web/packages/ui/layouts/AdminLayout.tsx
-
 import React, { ReactNode } from "react";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
@@ -12,17 +10,13 @@ interface AdminLayoutProps {
 export const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
   return (
     <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
-      {/* */}
+      {/* Admin Sidebar */}
       <AdminSidebar />
 
+      {/* Main content */}
       <div className="flex flex-col flex-1">
-        {/* */}
         <Header />
-
-        {/*  */}
         <main className="flex-1 p-6 overflow-auto">{children}</main>
-
-        {/*  */}
         <Footer />
       </div>
     </div>
