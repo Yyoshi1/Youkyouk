6c6df2ccc550eae865e553b0bd1cd1a5e860d221
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 06:40:39 +0100

    Update inbox.tsx

--- apps/web/pages/inbox.tsx
+++ apps/web/pages/inbox.tsx
@@ -1,13 +1,13 @@
 import { MainLayout } from "../packages/ui/layouts/MainLayout";
 import { useLicenseGuard } from "../packages/ui/license/useLicenseGuard";
 
-export const InboxPage: React.FC = () => {
+const InboxPage: React.FC = () => {
   useLicenseGuard();
 
   return (
     <MainLayout>
       <h1 className="text-2xl font-bold mb-4 text-gray-900 dark:text-white">Inbox</h1>
-      {/* */}
+      {/*  */}
     </MainLayout>
   );
 };
