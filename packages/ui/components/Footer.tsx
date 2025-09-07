a53dc640971188ae72374c9be6f8a3c2b5d99595
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 23:52:33 +0100

    Update Footer.tsx

--- packages/ui/components/Footer.tsx
+++ packages/ui/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="p-4 bg-white dark:bg-gray-800 shadow text-center text-gray-500 dark:text-gray-400">
+    <footer className="p-4 bg-white dark:bg-gray-800 shadow text-center text-sm text-gray-500 dark:text-gray-400">
       © {new Date().getFullYear()} Youkyouk. All rights reserved.
     </footer>
   );
