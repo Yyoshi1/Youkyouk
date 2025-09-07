c3eea54d5d875d4d5a81b8c4374e26e4c9912e6b
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 00:07:27 +0100

    Update Button.tsx

--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 
-interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: () => void;
-}
-
-export const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-    >
-      {children}
-    </button>
-  );
-};
+export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
+  children,
+  className = "",
+  ...props
+}) => (
+  <button
+    {...props}
+    className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ${className}`}
+  >
+    {children}
+  </button>
+);
