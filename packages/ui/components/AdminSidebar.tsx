14c3d78291bcd4db8a91532b73ad9c6d6b955458
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 23:53:14 +0100

    Update AdminSidebar.tsx

--- packages/ui/components/AdminSidebar.tsx
+++ packages/ui/components/AdminSidebar.tsx
@@ -1,42 +1,63 @@
 // packages/ui/components/AdminSidebar.tsx
 import React from "react";
-import { FaTachometerAlt, FaUsers, FaCogs, FaEnvelope, FaTasks } from "react-icons/fa";
+import { InboxIcon, IssuesIcon, PulseIcon, WorkspaceIcon, TeamsIcon, FavoritesIcon } from "../icons/Icons";
 
 export const AdminSidebar: React.FC = () => {
   return (
-    <aside className="w-64 bg-white dark:bg-gray-900 shadow flex flex-col h-screen">
-      <nav className="flex-1 overflow-auto">
-        <ul className="space-y-2 p-4">
-          {/* Dashboard */}
-          <li className="flex items-center space-x-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded cursor-pointer">
-            <FaTachometerAlt />
-            <span>Dashboard</span>
-          </li>
+    <aside className="w-64 bg-white dark:bg-gray-900 h-screen p-4 shadow">
+      <nav className="space-y-2">
+        <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+          <InboxIcon className="w-5 h-5" />
+          <span>Inbox</span>
+        </div>
+        <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+          <IssuesIcon className="w-5 h-5" />
+          <span>My Issues</span>
+        </div>
+        <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+          <PulseIcon className="w-5 h-5" />
+          <span>Pulse</span>
+        </div>
 
-          {/* Users */}
-          <li className="flex items-center space-x-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded cursor-pointer">
-            <FaUsers />
-            <span>Users</span>
-          </li>
+        <div className="mt-4 font-semibold">Workspace</div>
+        <div className="ml-2 space-y-1">
+          <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+            <WorkspaceIcon className="w-5 h-5" />
+            <span>Initiatives</span>
+          </div>
+          <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+            <WorkspaceIcon className="w-5 h-5" />
+            <span>Projects</span>
+          </div>
+          <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+            <WorkspaceIcon className="w-5 h-5" />
+            <span>Views</span>
+          </div>
+          <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+            <WorkspaceIcon className="w-5 h-5" />
+            <span>Members</span>
+          </div>
+        </div>
 
-          {/* Settings */}
-          <li className="flex items-center space-x-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded cursor-pointer">
-            <FaCogs />
-            <span>Settings</span>
-          </li>
+        <div className="mt-4 font-semibold">Your Teams</div>
+        <div className="ml-2 space-y-1">
+          <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+            <TeamsIcon className="w-5 h-5" />
+            <span>Team Alpha</span>
+          </div>
+          <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+            <TeamsIcon className="w-5 h-5" />
+            <span>Team Beta</span>
+          </div>
+        </div>
 
-          {/* Inbox */}
-          <li className="flex items-center space-x-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded cursor-pointer">
-            <FaEnvelope />
-            <span>Inbox</span>
-          </li>
-
-          {/* My Issues */}
-          <li className="flex items-center space-x-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded cursor-pointer">
-            <FaTasks />
-            <span>My Issues</span>
-          </li>
-        </ul>
+        <div className="mt-4 font-semibold">Favorites</div>
+        <div className="ml-2">
+          <div className="flex items-center space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded cursor-pointer">
+            <FavoritesIcon className="w-5 h-5" />
+            <span>Starred Project</span>
+          </div>
+        </div>
       </nav>
     </aside>
   );
