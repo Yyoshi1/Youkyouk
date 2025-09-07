62393fa3dde86acaa9da3d7899b2b2effebbc50e
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 00:28:54 +0100

    Update Input.tsx

--- packages/ui/components/Input.tsx
+++ packages/ui/components/Input.tsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({
-  className = "",
-  ...props
-}) => (
-  <input
-    {...props}
-    className={`px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-500 ${className}`}
-  />
+export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => (
+  <input {...props} className="px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-500" />
 );
