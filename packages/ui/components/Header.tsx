e276558617fb46cf53acc2be864d65155feadeac
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 15:47:27 +0100

    Update Header.tsx

--- packages/ui/components/Header.tsx
+++ packages/ui/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Header: React.FC = () => {
   return (
-    <header className="p-4 bg-gray-100 dark:bg-gray-800">
+    <header className="bg-white dark:bg-gray-800 p-4 shadow">
       <h1 className="text-xl font-bold text-gray-900 dark:text-white">Header</h1>
     </header>
   );
