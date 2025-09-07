00cbe184fd8702d6c8d7a863e897ec781da1f2fd
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 16:00:13 +0100

    Create Footer.tsx

---
+++ apps/web/packages/ui/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="bg-white border-t p-4 text-center text-gray-500">
+      © 2025 Youkyouk. All rights reserved.
+    </footer>
+  );
+};
