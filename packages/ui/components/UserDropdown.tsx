77544736816caf1b1089bfbd3024527836eb84a5
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 00:05:43 +0100

    Update UserDropdown.tsx

--- packages/ui/components/UserDropdown.tsx
+++ packages/ui/components/UserDropdown.tsx
@@ -1,26 +1,15 @@
-// packages/ui/components/UserDropdown.tsx
-import React, { useState } from "react";
+import React from "react";
 
 export const UserDropdown: React.FC = () => {
-  const [open, setOpen] = useState(false);
-
   return (
     <div className="relative">
-      <img
-        src="/icons/user.png"
-        alt="User"
-        className="w-8 h-8 rounded-full cursor-pointer"
-        onClick={() => setOpen(!open)}
-      />
-      {open && (
-        <div className="absolute right-0 mt-2 w-48 bg-white dark:bg-gray-800 shadow rounded">
-          <ul className="py-2">
-            <li className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">Profile</li>
-            <li className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">Settings</li>
-            <li className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">Logout</li>
-          </ul>
-        </div>
-      )}
+      <button className="flex items-center p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">
+        <img
+          src="/icons/user.png"
+          alt="User"
+          className="h-6 w-6 rounded-full"
+        />
+      </button>
     </div>
   );
 };
