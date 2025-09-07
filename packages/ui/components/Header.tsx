436284af1f4ffe0422d286719ade9b7811cfe3ff
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 15:45:03 +0100

    Create Header.tsx

---
+++ packages/ui/components/Header.tsx
@@ -0,0 +1,10 @@
+// packages/ui/components/Header.tsx
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <header className="p-4 bg-gray-100 dark:bg-gray-800">
+      <h1 className="text-xl font-bold text-gray-900 dark:text-white">Header</h1>
+    </header>
+  );
+};
