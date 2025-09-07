7f59e71271a7aabda2001520b45fa3fbfaa3efb5
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-07 23:46:07 +0100

    Update AddIcon.tsx

--- packages/ui/icons/general/AddIcon.tsx
+++ packages/ui/icons/general/AddIcon.tsx
@@ -1,1 +1,7 @@
+import React from "react";
 
+export const AddIcon: React.FC<{ className?: string }> = ({ className }) => (
+  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4"/>
+  </svg>
+);
