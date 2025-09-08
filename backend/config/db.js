dbcb1621dafa98057aba6b92edd499eaf80662e4
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 20:01:22 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-// هنا نستعمل SQLite بلا node-gyp
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './youkyouk.db',
+  storage: './youkyouk.db', // هاد الملف غادي يتخلق تلقائياً
   logging: false
 });
