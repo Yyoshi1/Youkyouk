ce7d1b480da7efab37d463e42756ef4f8e93bf15
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 14:46:06 +0100

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import i18n from './languages/i18n'
+import i18n from './languages/i18n.js'
+
+import './style.css'
 
 const app = createApp(App)
 app.use(i18n)
