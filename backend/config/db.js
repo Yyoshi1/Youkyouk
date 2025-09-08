3df83d6f775544a8a5339d2215358df672c6c57c
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 20:03:42 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './youkyouk.db', // هاد الملف غادي يتخلق تلقائياً
+  dialect: "sqlite",
+  storage: "./youkyouk.db", // الملف ديال قاعدة البيانات
   logging: false
 });
