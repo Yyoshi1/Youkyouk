b39711afe0754679efde3ecded1cca410086fbfb
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 16:15:08 +0100

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { register, login } from '../controllers/authController.js';
+import User from '../models/User.js';
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+// 
+router.post('/login', async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ where: { email } });
+  if (!user) return res.status(404).json({ message: 'User not found' });
+  res.json({ message: 'Login successful', user });
+});
 
 export default router;
