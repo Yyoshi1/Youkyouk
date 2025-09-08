e5161f06dca37ae4ccb91592318cb94a32e99ecc
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 18:16:12 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,12 +2,10 @@
 
 // تسجيل مستخدم جديد
 export const registerUser = (req, res) => {
-  // منطق التسجيل
   res.json({ message: 'User registered successfully' });
 };
 
 // تسجيل الدخول
 export const loginUser = (req, res) => {
-  // منطق تسجيل الدخول
   res.json({ message: 'User logged in successfully' });
 };
