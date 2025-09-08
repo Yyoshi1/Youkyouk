64cce5211c5af6177b293157897b496acccd13b3
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 16:12:02 +0100

    Update project.js

--- backend/models/project.js
+++ backend/models/project.js
@@ -1,24 +1,15 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/db.js';
+import Team from './Team.js';
 
 const Project = sequelize.define('Project', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  },
-  teamName: {
-    type: DataTypes.STRING,
-    allowNull: true
-  }
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.TEXT },
+  teamId: { type: DataTypes.INTEGER, references: { model: Team, key: 'id' } }
+}, {
+  tableName: 'projects',
+  timestamps: true
 });
 
 export default Project;
