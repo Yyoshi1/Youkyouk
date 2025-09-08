c3ef00340f2d4bcd7117becb92be9a707670ebf8
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 16:51:32 +0100

    Create Team.vue

---
+++ frontend/pages/Team.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="teams">
+    <h1>Teams</h1>
+    <ul>
+      <li v-for="team in teams" :key="team.id">{{ team.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Teams',
+  data() {
+    return {
+      teams: [
+        { id: 1, name: 'Team Alpha' },
+        { id: 2, name: 'Team Beta' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.teams {
+  padding: 1rem;
+}
+</style>
