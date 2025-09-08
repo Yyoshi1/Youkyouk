415d371d0df8c4a05a629cb072010c32b58efa05
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 20:26:09 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,22 +1,18 @@
-import { sequelize } from '../config/db.js';
+import sequelize from '../config/db.js';
 import { DataTypes } from 'sequelize';
 
-const User = sequelize.define(
-  "User",
-  {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    }
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
-  {
-    timestamps: true
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
   }
-);
+}, {
+  timestamps: true
+});
 
 export default User;
