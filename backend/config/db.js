789e23032fda5a497e930c2ba5650c282f1fc5c9
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 19:26:35 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./youkyouk.db", // الملف اللي حددتيه في database.js
+  storage: "./youkyouk.db",
   logging: false
 });
