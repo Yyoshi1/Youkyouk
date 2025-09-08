23f01d2e73170f7bbd3f5c20370ef4a4f52ef356
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 16:47:02 +0100

    Create Layout.vue

---
+++ frontend/components/Layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="layout">
+    <YoukyoukSidebar />
+    <div class="main-content">
+      <YoukyoukHeader />
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import YoukyoukHeader from './Header.vue'
+import YoukyoukSidebar from './Sidebar.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    YoukyoukHeader,
+    YoukyoukSidebar
+  }
+}
+</script>
+
+<style scoped>
+.layout {
+  display: flex;
+}
+
+.main-content {
+  flex: 1;
+  padding: 1rem;
+}
+</style>
