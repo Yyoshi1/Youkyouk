7c46affce761bea35528070ae784e871f7343865
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 16:14:21 +0100

    Update Notification.js

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -1,24 +1,15 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/db.js';
+import User from './User.js';
 
 const Notification = sequelize.define('Notification', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
-  message: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  read: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false
-  },
-  userId: {
-    type: DataTypes.UUID,
-    allowNull: false
-  }
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  message: { type: DataTypes.STRING, allowNull: false },
+  userId: { type: DataTypes.INTEGER, references: { model: User, key: 'id' } },
+  read: { type: DataTypes.BOOLEAN, defaultValue: false }
+}, {
+  tableName: 'notifications',
+  timestamps: true
 });
 
 export default Notification;
