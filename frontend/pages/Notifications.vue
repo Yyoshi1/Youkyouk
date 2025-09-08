68e5e53435eb08ad95ed1f9005dcb9ed85c9b23c
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 16:26:30 +0100

    Create Notifications.vue

---
+++ frontend/pages/Notifications.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="notifications-page">
+    <h1>Notifications</h1>
+    <ul>
+      <li v-for="note in notifications" :key="note.id">
+        {{ note.message }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotificationsPage',
+  data() {
+    return {
+      notifications: [
+        { id: 1, message: 'Welcome to Youkyouk!' },
+        { id: 2, message: 'Your first task is assigned.' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.notifications-page {
+  padding: 2rem;
+}
+</style>
