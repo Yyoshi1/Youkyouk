306eedc3c193564bef4b898a4b8bf126a0e1c4c5
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 14:25:47 +0100

    Create App.vue

---
+++ frontend/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="{ dark: isDark }">
+    <Header @toggleTheme="toggleTheme"/>
+    <div class="flex">
+      <Sidebar/>
+      <main class="flex-1">
+        <router-view/>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue'
+
+export default {
+  components: { Header, Sidebar },
+  data() {
+    return { isDark: false }
+  },
+  methods: {
+    toggleTheme() {
+      this.isDark = !this.isDark
+    }
+  }
+}
+</script>
