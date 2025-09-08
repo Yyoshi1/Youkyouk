5cde394a0a9ec0cf3a2a52837a7b7aed974497a5
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 19:33:37 +0100

    Update Notification.js

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.js';
+import { sequelize } from '../config/db.js';
 import User from './User.js';
 
 const Notification = sequelize.define('Notification', {
