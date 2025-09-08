5e0cce5b1e6f0447bc5626e56949f66937a1c032
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 20:25:44 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,8 @@
 import { Sequelize } from 'sequelize';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
-const dbPromise = open({
-  filename: './database.sqlite',
-  driver: sqlite3.Database
-});
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false
+  storage: './database.sqlite'
 });
 
-export { sequelize, dbPromise };
+export default sequelize;
