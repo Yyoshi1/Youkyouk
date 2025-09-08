eb624bf863c3cad8148dd8597b43dd91fbc928e1
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 19:38:34 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,14 @@
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../config/db.js';
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
 
-const User = sequelize.define('User', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.STRING, defaultValue: 'Member' }
-}, {
-  tableName: 'users',
-  timestamps: true
+export const User = sequelize.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  }
 });
-
-export default User;
