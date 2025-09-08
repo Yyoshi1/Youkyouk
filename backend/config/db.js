2dd311d531d6313c49d35e62e334b5017eca8b0e
AbdelDoua <71140310+Yyoshi1@users.noreply.github.com>
2025-09-08 16:10:18 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'youkyouk_db',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASS || 'password',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: false,
+  }
+);
 
 export default sequelize;
